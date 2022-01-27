da820819a9ac6fc113393a84170e11104c0f232b
Frédéric NOËL <contact@fredericnoel.com>
2022-01-27 12:10:20 +0100

    foreach

--- tableaux.php
+++ tableaux.php
@@ -5,9 +5,24 @@ include 'fonctions.php';
 $test = array(true, 12, 'toto');
 $eleves = array('Michel', $test, 'Patrick', 'Bob');
 $ages = [56, 42, 20, $eleves];
+//array_push($ages, 56);
+sort($ages);
 
-dump($ages);
+/* dump($ages);
+* echo afficherTableau($ages);
+* echo factorielle(10);
+*/
 
-echo afficherTableau($ages);
+$stagiaires = [
+    'Mathilde' => 27,
+    'Pierre' => 30,
+    'Raymonde' => 72
+];
 
-echo factorielle(10);
+$stagiaires['Jean-Louis'] = 48;
+
+dump($stagiaires);
+
+foreach ($stagiaires as $cle => $valeur) {
+    echo $cle . ' a ' . $valeur . ' ans';
+}
